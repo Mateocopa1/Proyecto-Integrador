970f59f693e954a97332b1daa9e6a26f9a0331ac
Mateo Valencia Sanchez <mateocopa1@hotmail.com>
2023-08-29 21:25:17 -0500

    Actualizacion de nombres

--- main.py
+++
@@ -1,3 +0,0 @@
-print('Bienvenido a nuestro juego por favor ingresa tu nombre para comensar ')
-name = input()
-print(f'Bienvenido {name}, es hora de comenzar te agradecemos que te halla unido')
--- proyec_int_1.py
+++ proyec_int_1.py
@@ -1,17 +1,3 @@
-from typing import List
-from typing import Tuple
-
-def convert_map(map_str: str) -> List[List[str]]:
-    rows = map_str.split("\n")
-    map_matrix = [list(row) for row in rows]
-    return map_matrix
-
-
-def clear_screen():
-    print("\033[2J\033[H", end="")
-
-    import readchar
-    
-    print("ingrese la letra: ")
-    key = input(readchar.readkey())
-    print(key)
+print('Bienvenido a nuestro juego por favor ingresa tu nombre para comensar ')
+name = input()
+print(f'Bienvenido {name}, es hora de comenzar te agradecemos que te halla unido')
